19e2c2fbd8648635d6e54148b0e22e33f8bf06f8
tienhoang <xuantien93@yahoo.com>
2023-06-20 01:59:30 -0400

    needs to work on signup

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -1,39 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { signUp } from "../../store/session";
 import './SignupForm.css';
 
 function SignupFormPage() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [errors, setErrors] = useState({});
+
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (password === confirmPassword) {
+  //     const data = await dispatch(signUp(username, email, password));
+  //     if (data) {
+  //       setErrors(data)
+  //     }
+  //   } else {
+  //     setErrors(['Confirm Password field must be the same as the Password field']);
+  //   }
+  // };
+
+  useEffect(() => {
+    const errors = {};
+    if (!emailValidation(email)) errors.email = "Not a valid email address";
+    if (!password || password.length < 6)
+      errors.password = "Password must be 6 characters";
+    if (password !== confirmPassword) errors.confirmPassword = "Password must match"
+    if (!username) errors.username = "Username is required"
+    setErrors(errors)
+  }, [username, email, password, confirmPassword]);
 
   if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-        const data = await dispatch(signUp(username, email, password));
-        if (data) {
-          setErrors(data)
-        }
-    } else {
-        setErrors(['Confirm Password field must be the same as the Password field']);
+
+    setSubmitted(true);
+
+    if (!Object.values(errors).length) {
+      if (password === confirmPassword) {
+        await dispatch(signUp(username, email, password));
+        return history.push("/");
+      }
     }
   };
+  // const emailValidation = (email) => {
+  //   return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  // };
+
+  // useEffect(() => {
+  //   const errors = {};
+  //   if (!firstName || firstName.trim().length > 50 || firstName.trim().length < 2)
+  //     errors.firstName = "First name must be between 2 characters and 50 characters";
+  //   if (!lastName || lastName.trim().length > 50 || lastName.trim().length < 2)
+  //     errors.lastName = "Last name must be between 2 characters and 50 characters";
+  //   if (!emailValidation(email)) errors.email = "Not a valid email address";
+  //   if (!password || password.length < 6)
+  //     errors.password = "Password must be 6 characters";
+  //   if (password !== confirmPassword) errors.confirmPassword = "Password must match"
+  //   if (!mobile || mobile.replace(/-/g, '').length !== 10) errors.mobile = "Must be a valid US Number"
+  //   setErrors(errors);
+  // }, [firstName, lastName, email, password, confirmPassword, mobile]);
+
+  const emailValidation = (email) => {
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  };
+
 
   return (
     <>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
+        {errors.firstName && submitted && (
+          <p className="sign-in-errors">{errors.email}</p>
+        )}
         <label>
           Email
           <input
@@ -43,6 +91,9 @@ function SignupFormPage() {
             required
           />
         </label>
+        {errors.firstName && submitted && (
+          <p className="sign-in-errors">{errors.username}</p>
+        )}
         <label>
           Username
           <input
@@ -52,6 +103,9 @@ function SignupFormPage() {
             required
           />
         </label>
+        {errors.firstName && submitted && (
+          <p className="sign-in-errors">{errors.password}</p>
+        )}
         <label>
           Password
           <input
@@ -61,6 +115,9 @@ function SignupFormPage() {
             required
           />
         </label>
+        {errors.firstName && submitted && (
+          <p className="sign-in-errors">{errors.confirmPassword}</p>
+        )}
         <label>
           Confirm Password
           <input
