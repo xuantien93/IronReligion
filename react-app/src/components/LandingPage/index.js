36f66ea1dc658a15e9de843ae2d67b6efd5a8260
tienhoang <xuantien93@yahoo.com>
2023-06-23 23:17:49 -0400

    css3

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -72,12 +72,22 @@ const LandingPage = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+    const toggleCollapse = (event) => {
+        let faqContent = event.currentTarget;
+        let collapseDiv = faqContent.querySelector(".faq-collapse");
+        let xButton = faqContent.querySelector(".x-btn");
+
+        collapseDiv.classList.toggle("collapsed");
+        xButton.classList.toggle("rotate-225");
+        faqContent.classList.toggle("bounce-animation");
+    };
+
     return (
         <div className='landing-page-container'>
             <div className='sub-landing-page'>
                 <div className='landing-page-intro'>
                     <h1>Join The Iron Elite</h1>
-                    <h3>Unleash your potential, redefine your limits, and sculpt the body of a warrior. With an unwavering dedication to excellence, we empower individuals to rise above and beyond, transforming both mind and body.</h3>
+                    <h3>Unleash your potential, redefine your limits, and forge a formidable physique. With an unwavering dedication to excellence, we empower individuals to rise above and beyond, transforming both mind and body.</h3>
                     <button id="try-us-btn" onClick={(e) => history.push("/signup")}>Join the <span className="fading-text">culture</span></button>
                 </div>
             </div>
@@ -186,6 +196,150 @@ const LandingPage = () => {
                 </div>
                 <div className='frequently-asked-questions'>
                     <h2>Frequently Asked Questions</h2>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>How old do I have to be to workout at Iron Religion? </h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span id="first-answer">15 and Over.</span>
+                                <span><span className='bold-words'>-16 and 17 Years Old:</span> Parent or legal guardian needs to sign Liability Waiver, after that is signed, individual can come in on their own every visit thereafter.</span>
+                                <span>*if legal guardian is signing we need proof of guardianship.</span>
+                                <span><span className='bold-words'>-15 Years Old:</span> Parent or legal guardian needs to sign waiver and remain on premises while individual works out.</span>
+                                <span><span className='bold-words'>-14 Years Old and under:</span> Parent or legal guardian needs to sign waiver and remain on premises while individual works out.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>Where do I park?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>Our main lot is located at: 1602 Industrial Dr, Dallas, TX 76107, United States</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>Can I tour the facility?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>You are welcome to tour the facility alongside a sales representative who is more than happy to discuss the different options we have available.</span>
+                                <span>Our sales representatives are available in person from 8am-9pm Monday through Friday, and 8am-6pm Saturdays and
+                                    Sundays! If you have any questions please email sales@thealphaletegym.com for our membership options!</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>Do you guys allow filming in your facility?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>Yes we allow filming! Make sure your filming is courteous to other's space and not blocking any walkways or emergency exits. If you are requiring additional lighting, stands, etc please speak to a manager about setting up an appointment.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>Do you have online waivers I could sign and either email or print out upon arrival?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>Unfortunately no, to insure the validity of the signed liability waiver, we will need a parent/legal guardian to physically sign the liability waiver.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>Could my friend sign the waiver for me if I'm under 18?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>No, it would have to be a parent or legal guardian to sign the liability waiver</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>If I purchase my day pass online, will the time expire after 24 hours?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>If you purchase a single, 3, or 7-day pass online, your time window doesn't start until you come to see us at our front desk! One of our associates will have you build a profile, sign our liability waiver, and appropriate day pass waiver. For 3 and 7-day passes a profile a photo is required and you will receive a key-tag upon purchase that you will use to scan-in at our front desk for each of your visits during your pass.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>How long are the Day Passes good for?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>- Our Single Day Pass is valid for 24 hours after time of purchase, the copy of the receipt is your ticket back for re-entry during that timeframe.</span>
+                                <span>- 3-day pass is valid for 72 hours after purchase. You will receive unlimited access for 72 hours. The profile photo is needed and a key-tag is given upon purchase that you will scan in at our front desk for all your check-ins during your visits. No partial refunds for days not utilized for pass purchase.</span>
+                                <span> *for online purchases your time-stamp doesn't start until you
+                                    Finish checking in at our front desk.</span>
+                                <span>- Our new 7-day pass is valid for 168 hours after purchase. You will receive unlimited access for 168 hours. The profile photo is needed and a key-tag is given upon purchase that you will scan in at our front desk for all your check-ins during your visits. No partial refunds for days not utilized for pass purchase</span>
+                                <span>*for online purchases, your time-stamp doesn't start until you finish checking in at our front desk.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>What are your hours?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>24/7</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>How can I cancel my membership?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>We handle cancellations via email! the website cancellation link located at the bottom of the page directly sends a ticket to our inbox! </span>
+                                <span>-You can also email <span className="bold-words">memberships@ironreligion.com</span>if you have any questions regarding your cancellation.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='faq-content' onClick={toggleCollapse} >
+                        <div className='faq-title' >
+                            <h3>Can I pose in the Studio?</h3>
+                            <button className='x-btn'>+</button>
+                        </div>
+                        <div className="faq-collapse" id="faq-collapse">
+                            <div className='line2'></div>
+                            <div className='faq-answers'>
+                                <span>Yes! We require appropriate attire to be worn during posing. If you are posing in bikini, please reserve your spot at the Front Desk with one of our associates.</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
