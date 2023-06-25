fd665c193c3d48fcee59d3aeea0cc6b5586a91e2
tienhoang <xuantien93@yahoo.com>
2023-06-25 19:31:05 -0400

    Css

--- app/seeds/classes.py
+++ app/seeds/classes.py
@@ -8,19 +8,19 @@ fake = Faker()
 
 def seed_classes():
     class1 = Class(
-        class_name ="Iron Titans: Total Mass Blast", trainer_id = 1, time_start=datetime(2023,6,19,hour = 9,minute = 30), time_end= datetime(2023,6,19, hour = 10, minute = 30),
+        class_name ="Iron Titans: Total Mass Blast", trainer_id = 1, time_start=datetime(2023,6,25,hour = 9,minute = 30), time_end= datetime(2023,6,25, hour = 10, minute = 30),
         created_at=fake.date_between(start_date='-5y', end_date='today'))
 
-    class2 = Class( class_name ="Muscle Mayhem: Power Pump", trainer_id = 2, time_start=datetime(2023,6,20,hour = 10,minute = 30), time_end= datetime(2023,6,20, hour = 11, minute = 30), created_at=fake.date_between(start_date='-5y', end_date='today'))
+    class2 = Class( class_name ="Muscle Mayhem: Power Pump", trainer_id = 2, time_start=datetime(2023,6,25,hour = 10,minute = 30), time_end= datetime(2023,6,25, hour = 11, minute = 30), created_at=fake.date_between(start_date='-5y', end_date='today'))
 
-    class3 = Class(class_name ="Sculpt & Shred: Bodybuilder's Playground",trainer_id = 3, time_start=datetime(2023,6,21,hour = 9,minute = 30), time_end= datetime(2023,6,21, hour = 10, minute = 30), created_at=fake.date_between(start_date='-5y', end_date='today'))
+    class3 = Class(class_name ="Sculpt & Shred: Bodybuilder's Playground",trainer_id = 3, time_start=datetime(2023,6,26,hour = 9,minute = 30), time_end= datetime(2023,6,26, hour = 10, minute = 30), created_at=fake.date_between(start_date='-5y', end_date='today'))
 
-    class4 = Class(class_name ="Massive Gains: Strength Fusion", trainer_id = 1, time_start=datetime(2023,6,22,hour = 8,minute = 30), time_end= datetime(2023,6,22, hour = 9, minute = 30),
+    class4 = Class(class_name ="Massive Gains: Strength Fusion", trainer_id = 1, time_start=datetime(2023,6,27,hour = 8,minute = 30), time_end= datetime(2023,6,27, hour = 9, minute = 30),
 created_at=fake.date_between(start_date='-5y', end_date='today'))
 
-    class5 = Class(class_name ="Ripped Revolution: Hardcore Muscle Build", trainer_id = 2, time_start=datetime(2023,6,23,hour = 12 ,minute = 30), time_end= datetime(2023,6,23, hour = 13, minute = 30),created_at=fake.date_between(start_date='-5y', end_date='today'))
+    class5 = Class(class_name ="Ripped Revolution: Hardcore Muscle Build", trainer_id = 2, time_start=datetime(2023,6,27,hour = 12 ,minute = 30), time_end= datetime(2023,6,27, hour = 13, minute = 30),created_at=fake.date_between(start_date='-5y', end_date='today'))
 
-    class6 = Class(class_name ="Iron Titans: Total Mass Blast", trainer_id = 3, time_start=datetime(2023,6,24,hour = 15,minute = 30),time_end= datetime(2023,6,24, hour = 16, minute = 30),
+    class6 = Class(class_name ="Iron Titans: Total Mass Blast", trainer_id = 3, time_start=datetime(2023,6,27,hour = 15,minute = 30),time_end= datetime(2023,6,27, hour = 16, minute = 30),
 created_at=fake.date_between(start_date='-5y', end_date='today'))
 
     classes = [class1,class2,class3,class4,class5,class6]
